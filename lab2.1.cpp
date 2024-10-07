b61b92d50440f5af5758dafa588bf7167b95a17e
baldej99 <nikita.sonic05@gmail.com>
2024-10-07 16:28:25 +0300

    Add files via upload

---
+++ lab2.1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b;
+    cin >> a >> b;
+    cout << (a + b) / 2 << endl;
+    cout << "Print +,-,* or /";
+    char op;
+    cin >> op;
+    /*if (op == '+') {
+        cout << a + b;
+    }
+    else if (op == '-') {
+        cout << a - b;
+    } 
+    else if (op == '*') {
+        cout << a * b;
+    }
+    else if (op == '/') {
+        if (b == 0) {
+            cout << "error";
+        }
+        else {
+            cout << a / b;
+        }
+    }
+    else {
+        cout << "wrong input";
+    }*/
+    switch (op) {
+    case '+':
+        cout << a + b;
+        break;
+    case '-':
+        cout << a - b;
+        break;
+    case '*':
+        cout << a * b;
+        break;
+    case '/':
+        if (b != 0) {
+            cout << a / b;
+        }
+        else {
+            cout << "error" << endl;
+        }
+        break;
+    default:
+        cout << "wrong input" << endl;
+        break;
+    }
+}
+
---
+++ lab2.2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    while (true) {
+        cout << "Введите положительное целое число: ";
+        cin >> n;
+        if (n > 0) {
+            break; 
+        }
+        else {
+            cout << "Сказано же, ПОЛОЖИТЕЛЬНОЕ ЧИСЛО\n";
+        }
+    }
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+    cout << sum << endl;
+    int arr[10] = { 2, 5, 8, 10, 12, 15, 20, 22, 25, 30 };
+    for (int i = 0; i < 10; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < 10; i ++) {
+        if (i % 2 == 0) {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << endl;
+    int oddSum = 0;
+    for (int i = 1; i < 10; i += 1) {
+        if (i % 2 == 1) {
+            oddSum += arr[i];
+        }
+    }
+    cout << oddSum;
+
+    return 0;
+}
+
