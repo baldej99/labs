585fea0615ba90de0ab5b3f29c3f317233e7569b
baldej99 <nikita.sonic05@gmail.com>
2024-10-20 20:52:52 +0300

    Add files via upload

---
+++ MyMath.h
@@ -0,0 +1,12 @@
+#pragma once
+class MyMath
+{
+public:
+    static int callCount;  
+
+    static int add(int a, int b);
+    static int sub(int a, int b);
+    static int mult(int a, int b);
+    static double div(double a, double b);
+};
+
