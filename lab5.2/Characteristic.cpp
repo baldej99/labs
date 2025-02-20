2bd987ea164f39c6a843bac57aef2e54a12dd158
baldej99 <nikita.sonic05@gmail.com>
2025-02-20 12:25:34 +0300

    Delete lab5.2 directory

--- lab5.2/Characteristic.cpp
+++
@@ -1,5 +0,0 @@
-#include "Characteristic.h"
-Characteristic::Characteristic(int strengthValue) : strength(strengthValue) {}
-int Characteristic::getDamage(const Weapon& weapon) const {
-    return strength + weapon.damage;
-}
