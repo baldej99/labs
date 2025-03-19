53116cd214d5ecb56c1ea07417c8119e42d0bd11
baldej99 <nikita.sonic05@gmail.com>
2025-03-19 22:24:11 +0300

    Add files via upload

---
+++ lab4pr.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include <unordered_set>
+#include <set>
+using namespace std;
+
+int main() {
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+
+    unordered_set<int> numbers;
+    int num;   
+    while (input >> num) {
+        if (num > 0) {
+            numbers.insert(num);
+        } else if (num < 0) {
+            numbers.erase(-num);
+        } else { // num == 0
+            set<int> sortedNumbers(numbers.begin(), numbers.end());
+            for (int value : sortedNumbers) {
+                output << value << " ";
+            }
+            output << endl;
+            break;
+        }
+    }
+    
+    input.close();
+    output.close();
+    return 0;
+}
