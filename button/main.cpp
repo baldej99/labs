68e05b10309c0b098146133636f363759f1f5597
baldej99 <nikita.sonic05@gmail.com>
2025-03-20 22:21:58 +0300

    Add files via upload

---
+++ button/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QPUshButton>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QPushButton *button = new QPushButton("Quit");
+    QObject::connect(button, SIGNAL(clicked()), &app, SLOT(quit()));
+    button->show();
+
+    return app.exec();
+}
