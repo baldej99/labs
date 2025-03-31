7621d6eb4fed9a261e7027446c7f2a4df11b1567
baldej99 <nikita.sonic05@gmail.com>
2025-03-31 12:23:51 +0300

    Add files via upload

---
+++ lab6pr.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+//6.1
+/*
+int dijkstra(const vector<vector<int>>& matrix, int s, int f) {
+    int n = matrix.size();
+    const int INF = 1e9;
+    vector<int> dist(n, INF);
+    vector<bool> used(n, false);
+    dist[s] = 0;
+
+    for (int i = 0; i < n; i++) {
+        int u = -1;
+        for (int j = 0; j < n; j++) {
+            if (!used[j] && (u == -1 || dist[j] < dist[u])) {
+                u = j;
+            }
+        }
+        if (u == -1 || dist[u] == INF)
+            break; 
+        used[u] = true;
+        for (int v = 0; v < n; v++) {
+            if (matrix[u][v] != -1 && dist[u] + matrix[u][v] < dist[v]) {
+                dist[v] = dist[u] + matrix[u][v];
+            }
+        }
+    }
+    return (dist[f] == INF ? -1 : dist[f]);
+}
+
+int main() {
+    int n, s, f;
+    cin >> n >> s >> f;
+    s--; 
+    f--; 
+    vector<vector<int>> matrix(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+    cout << dijkstra(matrix, s, f);
+    return 0;
+}
+*/
+//6.2
+
+const int INF = 1e9;
+
+int find_min_cost(int N, const vector<int>& fuel_cost, const vector<vector<int>>& graph) {
+    vector<int> dist(N, INF);
+    vector<bool> used(N);
+    dist[0] = 0; 
+
+    for (int step = 0; step < N; ++step) {
+        int u = -1;
+        for (int i = 0; i < N; ++i) {
+            if (!used[i] && (u == -1 || dist[i] < dist[u])) {
+                u = i;
+            }
+        }
+
+        if (u == -1 || dist[u] == INF)
+            break; 
+
+        used[u] = true;
+
+        for (int v : graph[u]) {
+            int cost = dist[u] + fuel_cost[u]; 
+            if (cost < dist[v]) {
+                dist[v] = cost;
+            }
+        }
+    }
+
+    return dist[N - 1] == INF ? -1 : dist[N - 1]; 
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> fuel_cost(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> fuel_cost[i];
+    }
+
+    int M;
+    cin >> M;
+    vector<vector<int>> graph(N);
+
+    for (int i = 0; i < M; ++i) {
+        int a, b;
+        cin >> a >> b;
+        a--; b--; 
+        graph[a].push_back(b);
+        graph[b].push_back(a); 
+    }
+
+    int result = find_min_cost(N, fuel_cost, graph);
+    cout << result << endl;
+
+    return 0;
+}
+
+
+
+
---
+++ lab7pr.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+struct Lecture {
+    int start;
+    int end;
+};
+
+void sortByEnd(Lecture arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j].end < arr[minIndex].end) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            Lecture temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+int maxLectures(Lecture arr[], int n) {
+    sortByEnd(arr, n); 
+
+    int count = 0;
+    int current_end = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (arr[i].start >= current_end) {
+            count++;
+            current_end = arr[i].end;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    Lecture lectures[1000]; 
+
+    for (int i = 0; i < N; ++i) {
+        cin >> lectures[i].start >> lectures[i].end;
+    }
+
+    int result = maxLectures(lectures, N);
+    cout << result << endl;
+
+    return 0;
+}
+*/
+
+const int MAX_N = 100000;
+const int INF = 1e9;
+
+struct Box {
+    int weight;
+    int capacity;
+};
+
+void selectionSort(Box arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        int minIndex = i;
+        int minValue = arr[i].weight + arr[i].capacity;
+        for (int j = i + 1; j < n; ++j) {
+            int sum = arr[j].weight + arr[j].capacity;
+            if (sum < minValue) {
+                minValue = sum;
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            Box temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    Box* boxes = new Box[MAX_N];
+    for (int i = 0; i < n; ++i) {
+        cin >> boxes[i].weight >> boxes[i].capacity;
+    }
+    selectionSort(boxes, n);
+
+    int* dp = new int[MAX_N + 1];
+    for (int i = 0; i <= n; ++i) dp[i] = INF;
+    dp[0] = 0;
+
+    int result = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int m = result; m >= 0; --m) {
+            if (dp[m] <= boxes[i].capacity) {
+                int newWeight = dp[m] + boxes[i].weight;
+                if (newWeight < dp[m + 1]) {
+                    dp[m + 1] = newWeight;
+                    if (m + 1 > result) {
+                        result = m + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << result << endl;
+    delete[] boxes;
+    delete[] dp;
+    return 0;
+}
+
